9036cd6056b98f5b199ec6d0fa7389e51ba6e97b
alramalho <alexandre.ramalho.1998@gmail.com>
2021-10-26 12:03:34 +0100

    Change terms of service navigation [#180076213]

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -47,38 +47,11 @@ export default function Home() {
           </div>
         </form>
 
-
-        {/*<div className="grid">*/}
-        {/*  <a href="https://nextjs.org/docs" className="card">*/}
-        {/*    <h3>Documentation &rarr;</h3>*/}
-        {/*    <p>Find in-depth information about Next.js features and API.</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a href="https://nextjs.org/learn" className="card">*/}
-        {/*    <h3>Learn &rarr;</h3>*/}
-        {/*    <p>Learn about Next.js in an interactive course with quizzes!</p>*/}
-        {/*  </a>*/}
-
-        {/*  <a*/}
-        {/*    href="https://github.com/vercel/next.js/tree/master/examples"*/}
-        {/*    className="card"*/}
-        {/*  >*/}
-        {/*    <h3>Examples &rarr;</h3>*/}
-        {/*    <p>Discover and deploy boilerplate example Next.js projects.</p>*/}
-        {/*  </a>*/}
-
-        {/*<a*/}
-        {/*  href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"*/}
-        {/*  className="button"*/}
-        {/*>*/}
-        {/*  <h3>Submit &rarr;</h3>*/}
-        {/*</a>*/}
-        {/*</div>*/}
       </main>
 
       <footer>
         <a
-          href="/terms-of-service"
+          href="/terms-of-service.html"
           rel="noopener noreferrer"
         >
           Terms of service
