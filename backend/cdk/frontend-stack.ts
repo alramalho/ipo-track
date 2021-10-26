38fdace5a6ae5d16c4f96f58ab7f82b047d70264
alramalho <alexandre.ramalho.1998@gmail.com>
2021-10-26 12:05:55 +0100

    Add frontend stack [#180076213]

---
+++ backend/cdk/frontend-stack.ts
@@ -0,0 +1,26 @@
+import * as cdk from '@aws-cdk/core';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as origins from '@aws-cdk/aws-cloudfront-origins';
+
+interface FrontendStackProps {
+  environment: string
+}
+
+export class FrontendStack extends cdk.Stack {
+
+  constructor(scope: cdk.Construct, id: string, props: FrontendStackProps) {
+    super(scope, id);
+
+    const bucket = new s3.Bucket(this, `Bucket-${props.environment}`, {
+      bucketName: `ipo-warning-s3-bucket-${props.environment}`
+    });
+    const distribution = new cloudfront.Distribution(this, `IPOWarningDistribution-${props.environment}`, {
+      defaultBehavior: { origin: new origins.S3Origin(bucket) },
+    });
+
+    new cdk.CfnOutput(this, `${props.environment}S3Arn`, {value: bucket.bucketArn});
+    new cdk.CfnOutput(this, `${props.environment}S3-S3Url`, {value: `s3://${bucket.bucketName}`});
+    new cdk.CfnOutput(this, `${props.environment}CloudFrontDistributionURL`, {value: `https://${distribution.distributionDomainName}`});
+  }
+}
--- backend/cdk/main-stack.ts
+++ backend/cdk/main-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import {DbStack} from "./db-stack";
 import {ApiStack} from "./api-stack";
+import {FrontendStack} from "./frontend-stack";
 
 interface MainStackProps {
   environment: string
@@ -11,6 +12,10 @@ export class MainStack extends cdk.Stack {
 
   constructor(scope: cdk.Construct, id: string, props: MainStackProps) {
     super(scope, id);
+
+    new FrontendStack(this, `FrontendStack-${props.environment}`, {
+      environment: props.environment
+    })
 
     const apiStack = new ApiStack(this, `ApiStack-${props.environment}`, {
       environment: props.environment,
