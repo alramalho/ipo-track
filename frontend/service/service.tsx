1d04e699721e4b90bcc7a38aa5ad928f3840d695
alramalho <alexandre.ramalho.1998@gmail.com>
2021-10-26 15:46:06 +0100

    Fix environment variables errors and add helping scripts

--- frontend/service/service.tsx
+++ frontend/service/service.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const subscribe = (data: FormData) =>
-     axios.post(`${process.env.API_URL}/subscribe`, {
+     axios.post(`${process.env.NEXT_PUBLIC_API_URL}/subscribe`, {
             email: data.email,
             keyword: data.keyword
         }
