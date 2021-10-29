d83def0fb6c5b3c00e17e701b01843b44de22819
alramalho <alexandre.ramalho.1998@gmail.com>
2021-10-29 22:37:04 +0100

    Uncomment domain configurations

--- backend/cdk/frontend-stack.ts
+++ backend/cdk/frontend-stack.ts
@@ -3,9 +3,10 @@ import {RemovalPolicy} from '@aws-cdk/core';
 import * as cloudfront from '@aws-cdk/aws-cloudfront';
 import * as s3 from '@aws-cdk/aws-s3';
 import * as origins from '@aws-cdk/aws-cloudfront-origins';
-// import * as acm from '@aws-cdk/aws-certificatemanager';
+import * as acm from '@aws-cdk/aws-certificatemanager';
 import * as route53 from '@aws-cdk/aws-route53';
-// import * as targets from '@aws-cdk/aws-route53-targets';
+
+import * as targets from '@aws-cdk/aws-route53-targets';
 
 
 interface FrontendStackProps {
@@ -22,41 +23,31 @@ export class FrontendStack extends cdk.Stack {
       bucketName: `ipo-warning-s3-bucket-${props.environment}`
     });
 
-    let distributionOptions:cloudfront.DistributionProps = {
-      defaultBehavior: {origin: new origins.S3Origin(bucket)},
-      defaultRootObject: "index.html",
-      errorResponses: [{httpStatus: 404, responsePagePath: '/404.html'}],
-      domainNames: inProduction ? ['www.ipo-warning.com'] : ['sandbox.ipo-warning.com']
-    };
-
     let myHostedZone = new route53.HostedZone(this, 'IpoWarningZone', {
       zoneName: 'ipo-warning.com',
     });
 
-    // const myCertificate = new acm.Certificate(this, 'Certificate', {
-    //   domainName: 'www.ipo-warning.com',
-    //   validation: acm.CertificateValidation.fromDns(myHostedZone),
-    // })
-    //
-    // // @ts-ignore
-    // distributionOptions['certificate'] = myCertificate
-    // // @ts-ignore
-    // distributionOptions['domainNames'] = ['www.ipo-warning.com']
-    //
-    // myCertificate.applyRemovalPolicy(RemovalPolicy.DESTROY)
-    // myHostedZone.applyRemovalPolicy(RemovalPolicy.DESTROY)
+    const myCertificate = new acm.Certificate(this, 'Certificate', {
+      domainName: 'www.ipo-warning.com',
+      validation: acm.CertificateValidation.fromDns(myHostedZone),
+    })
 
-    const distribution = new cloudfront.Distribution(this, `IPOWarningDistribution-${props.environment}`, distributionOptions)
-    //
-    // if (inProduction) {
-    //   new route53.ARecord(this, 'ARecord', {
-    //     recordName: "www.ipo-warning.com",
-    //     zone: myHostedZone,
-    //     target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
-    //   });
-    // }
+    const distribution = new cloudfront.Distribution(this, `IPOWarningDistribution-${props.environment}`, {
+      defaultBehavior: {origin: new origins.S3Origin(bucket)},
+      defaultRootObject: "index.html",
+      errorResponses: [{httpStatus: 404, responsePagePath: '/404.html'}],
+      domainNames: inProduction ? ['www.ipo-warning.com'] : ['sandbox.ipo-warning.com'],
+      certificate: inProduction ? myCertificate : undefined
+    })
 
+    new route53.ARecord(this, `ARecord-${props.environment}`, {
+      recordName: inProduction ? "www.ipo-warning.com" : "sandbox.ipo-warning.com",
+      zone: myHostedZone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
+    });
 
+    myCertificate.applyRemovalPolicy(RemovalPolicy.DESTROY)
+    myHostedZone.applyRemovalPolicy(RemovalPolicy.DESTROY)
     bucket.applyRemovalPolicy(RemovalPolicy.DESTROY)
     distribution.applyRemovalPolicy(RemovalPolicy.DESTROY)
 
