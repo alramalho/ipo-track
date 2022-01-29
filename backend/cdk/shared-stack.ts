dbdc336d6eed3db3430b5c5e4c85ce0c1a4fcca3
alramalho <alexandre.ramalho.1998@gmail.com>
2022-01-29 16:36:26 +0000

    Change hosted zone in Route 53

--- backend/cdk/shared-stack.ts
+++ backend/cdk/shared-stack.ts
@@ -12,7 +12,7 @@ export class SharedStack extends cdk.Stack {
     super(scope, id);
 
     this.hostedZone = route53.HostedZone.fromHostedZoneAttributes(this, 'SharedHostedZone', {
-      hostedZoneId: "Z06420601ZYM964XOXTOX",
+      hostedZoneId: "Z06380513UVH7N8Q9MJNO",
       zoneName: "ipo-track.com"
     })
     this.certificate = acm.Certificate.fromCertificateArn(this, `SharedCertificate`, "arn:aws:acm:us-east-1:854257060653:certificate/12c204cf-42b2-4daf-95a0-3209f7e4bbc4")
